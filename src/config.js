3f57f1b970c1670380d73ea39026c4db45e4bec5
cnbdg <3606604482@qq.com>
2026-03-14 18:25:38 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -30,8 +30,8 @@ const config = {
 			"type": "pic",   //"pic":静态壁纸;"video":动态壁纸
 			"datainfo": {
 				"title": "书房夜晚",
-				"preview": "/img/wallpaper/static/书房夜晚/image-pre.webp",
-				"url": "/img/wallpaper/static/书房夜晚/image.webp",     //当然，也可填写网络地址或壁纸api，如随机PC壁纸api："url":"https://t.mwm.moe/pc"
+				"preview": "/img/wallpaper/static/安逸舒适/image-pre.webp",
+				"url": "/img/wallpaper/static/安逸舒适/image.png",     //当然，也可填写网络地址或壁纸api，如随机PC壁纸api："url":"https://t.mwm.moe/pc"
 			},
 		},
 		"mobile": {   //移动端
